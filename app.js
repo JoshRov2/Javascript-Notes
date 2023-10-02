499badf4017c5bb65bc24bcfc8b3b0a979ebf378
JoshRov2 <joshr16@nycstudents.net>
2023-10-02 09:22:21 -0400

    notes1

---
+++ app.js
@@ -0,0 +1,31 @@
+// declare and define function, input output machines
+// we can pass in arguments or parameters.
+
+function test(mess) {
+    console.log("mess");
+}
+// run the funtion
+test("Josh");
+
+// string ""
+const x = "String";
+//declaring a variable, defining a variable
+test(x);
+
+//Number/Integer
+const y = 98;
+test(y);
+
+//Boolean true or false
+const z = true;
+test(z);
+
+const a = 32;
+test(a);
+
+const b = 47;
+test(b);
+
+function add(a,b){
+    console.log(x + y);
+}

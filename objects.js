ea26cb7bc6830bb48dd1a9d6c90b9e00035da995
JoshRov2 <joshr16@nycstudents.net>
2023-10-13 08:45:10 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 const date = new Date()
 let year = date.getFullYear
-const Dennis = {
+const students = {
     firstName: "Dennis",
     lastName: "Unknown",
     DOB: 2007,
@@ -8,11 +8,40 @@ const Dennis = {
     age: function () {
         return year - this.DOB;
     },
-};
-console.log(Dennis.firstName);
-
-const ChenZee = {
-    firstName: "ChenZee",
+    firstName: "Josh",
     lastName: "Unknown",
+    DOB: 2007,
+    graduated: false,
+    age: function () {
+        return year - this.DOB;
+    },
+    firstName: "Mark",
+    lastName: "Unknown",
+    DOB: 2007,
+    graduated: false,
+    age: function () {
+        return year - this.DOB;
+    },
+
+    firstName: "Paul",
+    lastName: "Unknown",
+    DOB: 2007,
+    graduated: false,
+    age: function () {
+        return year - this.DOB;
+    },
 }
-console.log(Dennis, ChenZee);
+let names = [Dennis, Josh, Mark, Paul];
+students.forEach((student) => console.log(student.age));
+
+
+
+
+
+
+
+let nums = [1, 2, 3, 4, 5];
+let movies = ["Star Wars", "Marvel Thing", "Barbie"];
+console.log(nums[nums.length - 1]);
+nums.forEach((el) => console.log(el));
+movies.forEach((movie) => console.log(movie));

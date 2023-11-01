4092ba48d8174dd54555f2f036976dde8b094475
JoshRov2 <joshr16@nycstudents.net>
2023-11-01 09:23:51 -0400

    notes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const DOMselectors = {
+/* const DOMselectors = {
 form: document.queryselector("#form"),
 firstName: document.querySelector(".first-name"),
 //grab all the text input
@@ -12,4 +12,32 @@ console.log(DOMselectors.firstName.value);
 DOMselectors.h2s.forEach(
 (el) => (el.textContent = DOMselectors.firstName.value)
 )
-});
+}); */
+
+for(i =0; i <= 7; i++){
+    console.log(i)
+}
+/* while(money < 7){
+    console.log(money);
+    money--;
+} */
+/* const numsArr = [];
+ */function linearSearch(num, arr){
+    let found;
+    for(let i =0; i < arr.length; i++){
+        if(arr[i] === num){
+            found = arr[i];
+            break;
+        }else{
+            console.log("still looking");
+        }
+    }
+    if(!found ){
+        console.log("sorry i couldn't find that")
+    }else{
+        console.log(found);
+    }
+    console.log(found);
+    return found;
+ }
+ linearSearch(5, [1,2,3,4,5,6,7,8,9,10]);

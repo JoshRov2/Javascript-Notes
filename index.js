17b7fe814a6126846ac18e6976e3aa02daab6c66
JoshRov2 <joshr16@nycstudents.net>
2023-11-02 09:23:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,30 +14,54 @@ DOMselectors.h2s.forEach(
 )
 }); */
 
-for(i =0; i <= 7; i++){
-    console.log(i)
-}
-/* while(money < 7){
-    console.log(money);
-    money--;
-} */
-/* const numsArr = [];
- */function linearSearch(num, arr){
-    let found;
-    for(let i =0; i < arr.length; i++){
-        if(arr[i] === num){
-            found = arr[i];
-            break;
-        }else{
-            console.log("still looking");
+// for(i =0; i <= 7; i++){
+//     console.log(i)
+// }
+// /* while(money < 7){
+//     console.log(money);
+//     money--;
+// } */
+// /* const numsArr = [];
+//  */function linearSearch(num, arr){
+//     let found;
+//     //for loop is iteration
+//     for(let i =0; i < arr.length; i++){
+//         //evaluations if statements, are selection
+//         if(arr[i] === num){
+//             found = arr[i];
+//             break;
+//         }else{
+//             console.log("still looking");
+//         }
+//     }
+//     if(!found ){
+//         console.log("sorry i couldn't find that")
+//     }else{
+//         console.log(found);
+//     }
+//     console.log(found);
+//     return found;
+//  }
+//  linearSearch(5, [1,2,3,4,5,6,7,8,9,10]);
+
+
+
+//PRACTICE
+/* define the amount of occupied spaces through a let or const, as N. N is the total amount of parking spaces.
+ */
+// "C" is an occupied space.
+// "." is an empty space.
+let y = "[C][C][.][C][.][C]"
+let t = "[C][.][.][.[C][C]"
+function spaces(s, y, t){
+    let total = 0;
+    for(let i = 0; i <= s; i++){
+        if(y[i] === "C" && t[i] === "C"){
+            total++;
         }
     }
-    if(!found ){
-        console.log("sorry i couldn't find that")
-    }else{
-        console.log(found);
-    }
-    console.log(found);
-    return found;
- }
- linearSearch(5, [1,2,3,4,5,6,7,8,9,10]);
+    console.log(total);
+}
+spaces(7, "CCCCCCC", "C.C.C.C");
+// N represents the amount of spaces available yesterday and today, so it will be used twice.
+

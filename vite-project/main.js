37a828dd165320c7ea494a08f04d8009bce82f0a
JoshRov2 <joshr16@nycstudents.net>
2023-11-30 09:24:27 -0500

    apis

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,24 +1,28 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+//go get latvian basketball data
+//show latvian basketball data
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/* function greet(name){
+  const greetPromise = new Promise(function(resolve, reject){
+    resolve(`Hello ${name}`);
+  });
+  return greetPromise;
+}
+const Aaron = greet("Aaron")
+console,log(Aaron);
+Aaron.then((result)=>{
+  console.log(result);
+}); */
 
-setupCounter(document.querySelector('#counter'))
+//REST API
+const URL = `https://api.quotable.io/random`;
+async function getData(URL){
+  try {
+    const response = await fetch(URL);
+    console.log(response);
+    const data = await response.json();
+    console.log(data)
+    document.querySelector("h1").textContent = data.content;
+    document.querySelector("h2").textContent = data.content;
+  } catch (error) {}
+};
+getData(URL);

f77f240644e2bd5ebc7a091b2cb16f22eb87d01d
JoshRov2 <joshr16@nycstudents.net>
2023-12-01 09:11:05 -0500

    Update main.js

--- vite-project/main.js
+++ vite-project/main.js
@@ -18,11 +18,16 @@ const URL = `https://api.quotable.io/random`;
 async function getData(URL){
   try {
     const response = await fetch(URL);
-    console.log(response);
+   /*  if(response.status != 200){
+      throw new Error(response.statusText);
+    }
+    console.log(response);   */  
     const data = await response.json();
     console.log(data)
     document.querySelector("h1").textContent = data.content;
     document.querySelector("h2").textContent = data.content;
-  } catch (error) {}
+  } catch (error) {
+    // document.querySelector("h1").textContent = error;
+  }
 };
 getData(URL);
